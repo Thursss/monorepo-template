aaff1e50c8abb8d9e21d8660e255d06608604f50
fry <1720557879@qq.com>
2025-06-11 12:01:13 +0800

    chore(m-main): 集成 generouted/react-router 实现微应用路由管理，新增布局组件和页面

--- apps/micro/_main/src/main.tsx
+++ apps/micro/_main/src/main.tsx
@@ -1,16 +1,10 @@
+import { Routes } from '@generouted/react-router'
 import microApp from '@micro-zoe/micro-app'
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
 import 'virtual:windi.css'
 import './index.css'
 
 microApp.start({
   'router-mode': 'native',
 })
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')!).render(<Routes />)
--- apps/micro/_main/vite.config.ts
+++ apps/micro/_main/vite.config.ts
@@ -1,10 +1,11 @@
+import generouted from '@generouted/react-router/plugin'
 import react from '@vitejs/plugin-react'
 import WindiCSS from 'vite-plugin-windicss'
 import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [WindiCSS(), react()],
+  plugins: [WindiCSS(), react(), generouted()],
   test: {
     globals: true,
     environment: 'happy-dom',
